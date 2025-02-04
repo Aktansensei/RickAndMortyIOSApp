30f745a0bca4b0cc68790b7287375444fec3ce47
Asanaliev Aktan <gobitheplayer@gmail.com>
2025-02-04 12:54:32 +0600

    Added models

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Актан on 4/2/25.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case female = "Female"
+    case male = "Male"
+    case genderless = "Genderless"
+    case unknown = "unknown"
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Актан on 4/2/25.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
